2d1c2e565396a4ff750ac5a29a85294bdca1a9eb
Kyle Lee <94151177+KyleKL@users.noreply.github.com>
2026-02-12 22:38:50 +0900

    Update create_admin.py 2

--- create_admin.py
+++ create_admin.py
@@ -4,10 +4,11 @@ from werkzeug.security import generate_password_hash
 with app.app_context():
     db.create_all()
     if not User.query.filter_by(username='admin').first():
-        hashed_pw = generate_password_hash('1234')
+        hashed_pw = generate_password_hash('060921')
         new_admin = User(username='admin', password=hashed_pw)
         db.session.add(new_admin)
         db.session.commit()
-        print("✅ 관리자 계정이 생성되었습니다! (admin / 1234)")
+        print("✅ 관리자 계정이 생성되었습니다! (admin / 060921)")
     else:
+
         print("⚠️ 이미 계정이 존재합니다.")
